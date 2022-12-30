b6821907823be8324a2428feef5c723cea707434
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2022-12-30 03:37:43 +0200

    Score added and some improvements to ork class

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ let stage, canvas;
 let w, h;
 let loader;
 let move_multiplier = 10;
-let orkSpeed = 50;
-let ork
 let pointFramerate = 10;
 let orkContainer
 let orksOnScreen = 10
 let spriteSheet
 let harvesters = new Map()
+let killedCounter = 0
+let killedText
+window.orks = {
+    orksToSpawn: []
+}
+window.orkStats = {
+    harm: 10,
+    speed: 50
+}
 
 
 function init() {
@@ -17,6 +24,7 @@ function init() {
     h = stage.canvas.height;
 
     canvas = stage.canvas
+
 
     let manifest = [
         {src: "point.png", id: "point"},
@@ -40,10 +48,20 @@ function handleComplete(event) {
 
     background.addEventListener('click', startHarvesters)
 
+    killedText = new createjs.Text(`Killed: ${killedCounter}`, "32px Arial", "#343434")
+    let killedTextBounds = killedText.getBounds()
+    killedText.x = w - killedTextBounds.width - 50
+    killedText.y = killedTextBounds.height / 2
+    setInterval(() => {
+        raid()
+    }, 10000)
+
     stage.addChild(background)
 
-
     orkContainer = new createjs.Container();
+
+
+    stage.addChild(killedText)
     stage.addChild(orkContainer);
 
     spriteSheet = new createjs.SpriteSheet({
@@ -56,14 +74,41 @@ function handleComplete(event) {
         }
     });
 
-    generateOrk()
+    for (let i = 0; i < orksOnScreen; i++) {
+        window.generateOrk()
+    }
+
     createjs.Ticker.timingMode = createjs.Ticker.TIMEOUT
-    createjs.Ticker.framerate = 60
+    createjs.Ticker.framerate = 27
     createjs.Ticker.addEventListener('tick', stage);
-    createjs.Ticker.addEventListener("tick", tick);
+    createjs.Ticker.addEventListener('tick', tick);
+}
+
+function raid() {
+    let text = new createjs.Text("RAID INCOMING", "86px Arial", "#343434")
+    text.x = w / 2;
+    text.y = h / 8;
+    let b = text.getBounds();
+    text.regX = b.width / 2
+    text.regY = b.height / 2
+    text.textBaseline = "alphabetic";
+    createjs.Tween.get(text)
+        .to( { scale: 0.7 }, 600 )
+        .to( { scale: 1 }, 600 )
+    stage.addChild(text)
+    setTimeout(() => { stage.removeChild(text) }, 5000)
+
+    let count = window.orks.orksToSpawn.length
+    for (let i = 0; i < count; i++) {
+        let ork = window.orks.orksToSpawn.shift()
+        window.orks[ork.id] = ork
+        orkContainer.addChild(ork)
+    }
 }
 
 function spawnToilet(event) {
+
+    console.log(window.orks)
     let x = event.stageX
     let y = event.stageY
 
@@ -75,6 +120,9 @@ function spawnToilet(event) {
     toilet.regX = toilet.image.width / 2
     toilet.regY = toilet.image.height / 2
     stage.addChild(toilet)
+
+    //TODO rework for window.orks
+    return
 
     let orksAlive = orkContainer.children
 
@@ -136,20 +184,20 @@ function startHarvesters() {
     stage.addChild(harvester4)
     stage.addChild(harvester5)
 
-    let collisionListener = createjs.Ticker.addEventListener('tick', handleHarvesterCollision)
+    let collisionListener = setInterval(handleHarvesterCollision, 100)
     for (let i = 0; i < 5; i++) {
         let harvesterInMap = createjs.Tween.get(harvesters.get(`harvester${i + 1}`))
 
         harvesterInMap
-            .to( { x: i % 2 === 0 ? w + 160 : -160 }, 5000 )
-        console.log(i)
+            .to( { x: i % 2 === 0 ? w + 160 : -160 }, 2000 )
         if (i === 4) {
             harvesterInMap
-                .to( { x: i % 2 === 0 ? w + 160 : -160 }, 5000 )
+                .to( { x: i % 2 === 0 ? w + 160 : -160 }, 2000 )
                 .addEventListener('complete', () => {
-                    createjs.Ticker.removeEventListener('tick', collisionListener)
+                    clearInterval(collisionListener)
                     stage.removeChild(harvesterInMap)
                     harvesters = new Map()
+                    console.log('clearing')
 
                     stage.removeChild(harvester)
                     stage.removeChild(harvester2)
@@ -162,7 +210,7 @@ function startHarvesters() {
 
 }
 
-function handleHarvesterCollision(event) {
+function handleHarvesterCollision() {
 
     harvesters.forEach(harvester => {
         let leftX = harvester.x - harvester.regX + 5
@@ -176,30 +224,28 @@ function handleHarvesterCollision(event) {
 
         for (let i = 0; i < points.length; i++) {
             let objects = stage.getObjectsUnderPoint(points[i].x, points[i].y)
-            if (objects.filter((object) => object.name === 'ork').length > 0) {
+            if (objects.filter((object) => object.name === 'ork' && object.state === 'alive').length > 0) {
                 let ork = objects.filter((object) => object.name === 'ork')[0]
-                ork.gotoAndPlay('click')
-                createjs.Tween.get(ork, { override: true })
-                    .to({ x: ork.x, y: ork.y })
-                setTimeout(() => {
-                    orkContainer.removeChild(ork)
-                }, 1000)
+                ork.kill()
+                window.generateOrk()
             }
         }
     })
 }
 
-function generateOrk() {
-    let currentOrks = orkContainer.children.length
+window.generateOrk = function generateOrk() {
+    let orks = Object.keys(window.orks).filter((orkKey) =>
+        window.orks[orkKey].state !== 'alive'
+    )
+    killedText.text = `Killed: ${orks.length - 1}`
 
-    for (currentOrks; currentOrks < orksOnScreen; currentOrks++) {
-        let newOrk = new Ork(spriteSheet, 'idle' , {
-            x: (w * Math.random()) + 100 > w ? w - 200 : (w * Math.random()) + 100,
-            y: (h * Math.random()) + 100 > h ? h - 200 : (h * Math.random()) + 100,
-            route: generateRandomRote(5) })
-        newOrk.move()
-        orkContainer.addChild(newOrk)
-    }
+    let newOrk = new Ork(spriteSheet, 'idle' , {
+        x: (w * Math.random()) + 100 > w ? w - 200 : (w * Math.random()) + 100,
+        y: (h * Math.random()) + 100 > h ? h - 200 : (h * Math.random()) + 100,
+        route: generateRandomRote(5) })
+    newOrk.move()
+    window.orks.orksToSpawn.push(newOrk)
+
 }
 
 function generateRandomRote(numberOfSteps) {
@@ -208,17 +254,6 @@ function generateRandomRote(numberOfSteps) {
         rote.push(`${50 * Math.random()}*${360 * Math.random()}`)
     }
     return rote
-}
-
-function handleClick (event) {
-    let point = event.target
-    point.gotoAndPlay('click')
-    setTimeout(() => {
-        stage.removeChild(point)
-    }, 1000)
-
-    createjs.Tween.get(point, { override: true })
-        .to( { x: point.x, y: point.y })
 }
 
 
